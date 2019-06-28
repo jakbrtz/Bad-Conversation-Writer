cdc93c8fb75a8f5a1d677a3c8812cd94929edee0
Jake <jake@bortz.com.au>
2019-06-28 23:29:40 +1000

    added status to bot

--- bot.py
+++ bot.py
@@ -1,6 +1,11 @@
 import discord
-from gpt_2_simple import gpt_2 as gpt2
+import gpt_2_simple as gpt2
 import string
+
+# Start the gpt session
+model_name = "117M"
+gpt2.download_gpt2(model_name=model_name)
+sess = gpt2.start_tf_sess()
 
 # Everything that the bot does is inside this class:
 class MyClient(discord.Client):
@@ -27,6 +32,7 @@ class MyClient(discord.Client):
 			if len(words) > 1:
 				channel = channel.guild.get_channel(int(words[1][2: len(words[1])-1]))
 			scrapeOutput = ""
+			await client.change_presence(activity=discord.Game("scrape " + channel.name))
 			async for msg in channel.history(limit = 10000):
 				if msg.content[:1] in string.punctuation:
 					continue
@@ -39,32 +45,33 @@ class MyClient(discord.Client):
 				scrapeOutput = msg.author.name + ": " + msg.content + "\n" + scrapeOutput
 			open("scrape.txt","w+", encoding="utf-8").write(scrapeOutput)
 			await message.channel.send("Done! Type `!train` to create a model. This will take a long time.")
+			await client.change_presence(activity=None)
 			print("done!")
 			
 		# once you have the data, train the model
 		if words[0] == "!train":
-			steps=10
+			steps=100
 			if len(words) > 1:
 				steps=int(words[1])
 			
+			await client.change_presence(activity=discord.Game("with a neural net"))
+			
 			gpt2.finetune(sess,
-              'scrape.txt',
+              'scrape.txt', 
               model_name=model_name,
               steps=steps)
 			
+			await client.change_presence(activity=None)
 			await message.channel.send("Done! Type `!generate` to get some text")
 			print("done!")
 			
 		# generate new textgen
 		if words[0] == "!generate":
-			output = gpt2.generate(sess)
-			await message.channel.send("```\n" + output + "\n```")
+			await client.change_presence(activity=discord.Game("Microsoft Word"))
+			output = gpt2.generate(sess, return_as_list=True)[0]
+			await client.change_presence(activity=None)
+			await message.channel.send("```\n" + output[:1990] + "\n```")
 			print("done")
-
-# Start the gpt session
-model_name = "attempt2"
-gpt2.download_gpt2(model_name=model_name)
-sess = gpt2.start_tf_sess()
 
 # Run the bot
 client = MyClient()
