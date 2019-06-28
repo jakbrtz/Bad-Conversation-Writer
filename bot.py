90377b4fb728b7bf14307a36c0f5ed67c16a7ded
Jake <jake@bortz.com.au>
2019-06-28 17:41:15 +1000

    put all code in one place

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import discord
+from gpt_2_simple import gpt_2 as gpt2
 import string
 
 # Everything that the bot does is inside this class:
@@ -25,18 +26,45 @@ class MyClient(discord.Client):
 			channel = message.channel
 			if len(words) > 1:
 				channel = channel.guild.get_channel(int(words[1][2: len(words[1])-1]))
-			includeMessage = True
 			scrapeOutput = ""
 			async for msg in channel.history(limit = 10000):
-				includeMessage = True
 				if msg.content[:1] in string.punctuation:
-					includeMessage = False
+					continue
 				if msg.content.strip() == '':
-					includeMessage = False
-				if includeMessage:
-					scrapeOutput = msg.author.name + ": " + msg.content + "\n" + scrapeOutput
+					continue
+				if "http" in msg.content: 
+					continue
+				if message.author.bot:
+					continue
+				scrapeOutput = msg.author.name + ": " + msg.content + "\n" + scrapeOutput
 			open("scrape.txt","w+", encoding="utf-8").write(scrapeOutput)
+			await message.channel.send("Done! Type `!train` to create a model. This will take a long time.")
 			print("done!")
+			
+		# once you have the data, train the model
+		if words[0] == "!train":
+			steps=10
+			if len(words) > 1:
+				steps=int(words[1])
+			
+			gpt2.finetune(sess,
+              'scrape.txt',
+              model_name=model_name,
+              steps=steps)
+			
+			await message.channel.send("Done! Type `!generate` to get some text")
+			print("done!")
+			
+		# generate new textgen
+		if words[0] == "!generate":
+			output = gpt2.generate(sess)
+			await message.channel.send("```\n" + output + "\n```")
+			print("done")
+
+# Start the gpt session
+model_name = "attempt2"
+gpt2.download_gpt2(model_name=model_name)
+sess = gpt2.start_tf_sess()
 
 # Run the bot
 client = MyClient()
